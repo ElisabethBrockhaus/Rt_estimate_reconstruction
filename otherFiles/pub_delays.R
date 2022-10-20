b40f47242ba998ea6925cf6fee56e670364cabfc
ElisabethBrockhaus <elisabeth.brockhaus@outlook.de>
2022-10-20 17:31:49 +0200

    include forecasts

--- otherFiles/pub_delays.R
+++ otherFiles/pub_delays.R
@@ -7,11 +7,8 @@ source("Rt_estimate_reconstruction/load_data.R")
 path_estimates <- "reproductive_numbers/data-processed/"
 
 # sources of published real-time estimates
-methods <- list.dirs(path_estimates, full.names = F)
-methods <- methods[!methods %in% c("", "AW_7day", "AW_WVday", "owid", "ETHZ_step",
-                                   "ETHZ_sliding_window_deaths", "ETHZ_step_deaths",
-                                   "zidatalab")]
-methods
+methods <- c("Braunschweig", "ETHZ_sliding_window", "RKI_7day",
+             "rtlive", "SDSC", "epiforecasts", "ilmenau", "globalrt_7d")
 
 n <- length(methods)
 init <- rep(NA, n)
@@ -27,11 +24,25 @@ for (method in methods) {
 min_lag <- 0
 max_lag <- 16
 
+start_default <- "2020-10-01"
+start_globalrt <- "2021-02-18"
+start_ilmenau <- "2020-11-19"
+
 for (method in methods){
   print(method)
   pub_dates <- list.files(paste0(path_estimates, method),
                           full.names = F) %>% substr(1, 10)
-  pub_dates <- pub_dates[which((pub_dates <= "2021-07-18") & (pub_dates >= "2020-11-16"))]
+  
+  if (method == "globalrt_7d"){
+    start <- start_globalrt
+  } else if (method == "ilmenau"){
+    start <- start_ilmenau
+  } else {
+    start <- start_default
+  }
+  end <- as.character(as_date(start) + weeks(10))
+  
+  pub_dates <- pub_dates[which((pub_dates <= end) & (pub_dates >= start))]
 
   for (country in c("DE", "AT", "CH")){
     print(country)
@@ -79,7 +90,7 @@ for (method in methods){
     num.est <- sum(min_pub_delays_list[[method]][, country])
     if(num.est > 0){
       cum.num.est <- cumsum(min_pub_delays_list[[method]][, country])
-      min.index <- which.max(cum.num.est/num.est >= 0.95)
+      min.index <- which.max(cum.num.est/num.est >= 0.7)
       pub_delays[method, country] <- row.names(min_pub_delays_list[[method]])[min.index]
     }
   }
