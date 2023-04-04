3bd1acda3e8f881d6d08989626aa51049bdeffe5
ElisabethBrockhaus <elisabeth.brockhaus@outlook.de>
2023-04-04 18:24:34 +0200

    adjust layout (sizes, format)

--- otherFiles/gtd_scatterplot.R
+++ otherFiles/gtd_scatterplot.R
@@ -47,39 +47,40 @@ gtd_scatterplot <- ggplot(data = gtd_scatter, aes(x = gt_mean, y = gt_sd, color 
                  aes(xmin = gt_mean - 2*0.7, xmax = gt_mean + 2*0.7),
                  alpha = .5, linetype=2) +
   
-  geom_point(size = 2) +
+  geom_point(size = 3) +
   geom_text(label = rownames(gtd_scatter),
             position = position_nudge(y = ifelse(rownames(gtd_scatter) %in% c("AT", "RKI"), +0.25,
                                                  ifelse(rownames(gtd_scatter) %in% c("ETH(CH)/SDSC/SE", "rtlive/DK/BE"), 0.02, -0.2)),
-                                      x = ifelse(rownames(gtd_scatter) == "ETH(CH)/SDSC/SE", 0.9,
-                                                 ifelse(rownames(gtd_scatter) == "rtlive/DK/BE", 0.6, 0))),
-            size = 4, family="serif") + 
+                                      x = ifelse(rownames(gtd_scatter) == "ETH(CH)/SDSC/SE", 1,
+                                                 ifelse(rownames(gtd_scatter) == "rtlive/DK/BE", 0.7, 0))),
+            size = 5, family="serif") + 
   scale_x_continuous(breaks = seq(0,10,2)) +
   scale_color_manual(values=c("#000000", "#909090")) +
   
   geom_point(data = data.frame(gt_mean=4, gt_sd=4), colour = "red", size = 5, shape = 18) + 
   
-  geom_point(data = AT_first, colour = "#909090", shape=21, size = 2) +
+  geom_point(data = AT_first, colour = "#909090", shape=21, size = 3) +
   geom_text(mapping = aes(x=AT_first$gt_mean, y=AT_first$gt_sd),
             label="(AT)", color="#909090",
-            nudge_y = -0.2, check_overlap = TRUE, size = 2.5,
+            nudge_y = -0.2, check_overlap = TRUE, size = 3.5,
             family="serif") +
   geom_segment(aes(x=AT_first$gt_mean, y=AT_first$gt_sd,
                    xend=gtds["AT","gt_mean"], yend=gtds["AT","gt_sd"]),
                arrow=arrow(length=unit(2.5, "mm")), color="#909090", size=0.01, linetype=5) +
   
-  geom_point(data = NL_first, colour = "#909090", shape=21, size = 2) +
+  geom_point(data = NL_first, colour = "#909090", shape=21, size = 3) +
   geom_text(mapping = aes(x=NL_first$gt_mean, y=NL_first$gt_sd),
             label="(NL)", color="#909090",
-            nudge_y = -0.2, check_overlap = TRUE, size = 2.5,
+            nudge_y = -0.2, check_overlap = TRUE, size = 3.5,
             family="serif") +
   geom_segment(aes(x=NL_first$gt_mean, y=NL_first$gt_sd,
                    xend=gtds["NL","gt_mean"], yend=gtds["NL","gt_sd"]),
                arrow=arrow(length=unit(2.5, "mm")), color="#909090", size=0.01, linetype=5) +
   
-  coord_cartesian(xlim=c(0,10.6), ylim=c(-0.2,7.9), expand=F)
+  coord_cartesian(xlim=c(0,10.6), ylim=c(-0.1,7.9), expand=F)
 
 print(gtd_scatterplot)
 
 ggsave(gtd_scatterplot, filename = "Figures/gtd_scatterplot.png",  bg = "transparent",
-       width = 10, height = 7.8)
+       width = 10, height = 6.5)
+
